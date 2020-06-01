f6678fca844ddf0fc9c1146c53ac16e364651c1b
caiofurtado <caiohbfurtado@gmail.com>
2020-06-01 09:57:38 -0300

    final version

--- src/Pages/InfoSerie/index.js
+++ src/Pages/InfoSerie/index.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import { Badge } from 'reactstrap';
 
 function InfoSerie({ match }) {
-  const [serieName, setSerieName] = useState('');
-  const [data, setData] = useState({});
   const history = useHistory();
+  const [form, setForm] = useState({});
+  const [data, setData] = useState({});
+  const [edit, setEdit] = useState(false);
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/genres')
+      .then(res => {
+        setGenres(res.data.data)
+      })
+  }, [])
+
+  console.log(genres);
 
   useEffect(() => {
     axios.get(`/api/series/${match.params.id}`).then(res => {
@@ -15,33 +28,108 @@ function InfoSerie({ match }) {
 
   useEffect(() => {
     axios.get(`/api/series/${match.params.id}`).then(res => {
-      setSerieName(res.data.name)
+      setForm(res.data)
     })
   }, [match.params.id])
 
-  const onChange = evt => {
-    setSerieName(evt.target.value);
+  const onChange = field => evt => {
+    setForm({
+      ...form,
+      [field]: evt.target.value
+    });
   }
 
   const save = () => {
-    axios.put(`/api/series/${match.params.id}`, { name: serieName }).then(res => {
-      history.push('/series');
+    axios.put(`/api/series/${match.params.id}`, form).then(res => {
+      history.push('/series/');
+    })
+  }
+
+  const customHeader = {
+    height: '50vh',
+    minHeight: '500px',
+    backgroundImage: `url('${data.background}')`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat'
+  }
+
+  const seleciona = value => () => {
+    setForm({
+      ...form,
+      status: value
     })
   }
 
   return (
-    <div className='container'>
-      <h1>Informações da Série:</h1>
-      <pre>{JSON.stringify(data)}</pre>
-      <form>
-        <div className="form-group">
-          <label htmlFor="name">Nome:</label>
-          <input type="text" value={serieName} onChange={onChange} className="form-control" id="name" />
+    <div>
+      <header style={customHeader}>
+        <div className='h-100' style={{ background: 'rgba(0,0,0,0.7)' }}>
+          <div className='h-100 container'>
+            <div className="row h-100 align-items-center">
+              <div className="col-3">
+                <img className='img-fluid img-thumbnail' src={data.poster} alt={data.name} />
+              </div>
+              <div className="col-9">
+                <h1 className='font-weight-light text-light'>{data.name}</h1>
+                <div className='lead text-white'>
+                  {data.status === 'Finalizado' && <Badge color='success'>Finalizado</Badge>} 
+                  {data.status === 'Assistindo' && <Badge color='warning'>Assistindo</Badge>}
+                </div>
+                <div className='lead text-white'>
+                  Gênero: {data.genre}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <button type="button" onClick={save} className="btn btn-primary">Salvar</button>
-        <button type="button" onClick={() => history.push('/series')} className="btn btn-danger ml-2">Voltar</button>
-      </form>
+      </header>
+      {
+        edit === false &&
+        <div className="container mt-3">
+          <button className='btn btn-primary' onClick={() => setEdit(!edit)}>Editar</button>
+          <button className='btn btn-danger ml-2' onClick={() => history.push('/series')}>Voltar Para Séries</button>
+        </div>
+      }
+      {
+        edit === true &&
+        <div className='container mt-3'>
+          <h1>Informações da Série:</h1>
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Nome:</label>
+              <input type="text" value={form.name} onChange={onChange('name')} className="form-control" id="name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="comments">Comentários:</label>
+              <input type="text" value={form.comments} onChange={onChange('comments')} className="form-control" id="comments" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="genres">Gêneros:</label>
+              <select selected={form.genre} onChange={onChange('genre_id')} className="custom-select custom-select-lg mb-3" value={data.genre_id}>
+                <option>Selecione um gênero</option>
+                {
+                  genres.map(genre => <option key={genre.id} value={genre.id}>{genre.name}</option>)
+                }
+              </select>
+            </div>
+            <div className="form-group">
+              <div className="custom-control custom-radio">
+                <input type="radio" id="assistindo" checked={form.status === 'Assistindo'} value='assistindo' name="status" className="custom-control-input" onChange={seleciona('Assistindo')}/>
+                <label className="custom-control-label" htmlFor="assistindo">Assistindo</label>
+              </div>
+              <div className="custom-control custom-radio">
+                <input type="radio" id="finalizado" checked={form.status === 'Finalizado'} value='finalizado' name="status" className="custom-control-input" onChange={seleciona('Finalizado')}/>
+                <label className="custom-control-label mb-3" htmlFor="finalizado">Finalizado</label>
+              </div>
+              <button type="button" onClick={save} className="btn btn-primary">Salvar</button>
+              <button type="button" onClick={() => setEdit(!edit)} className="btn btn-danger ml-2">Cancelar</button>
+            </div>
+          </form>
+        </div>
+      }
     </div>
+
   );
 }
 
--- src/Pages/NewSerie/index.js
+++ src/Pages/NewSerie/index.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 
 function NewSerie() {
-  const [serie, setSerie] = useState('');
+  const [form, setForm] = useState({
+    name: ''
+  });
+  const [genres, setGenres] = useState([]);
   const history = useHistory();
 
-  const onChange = evt => {
-    setSerie(evt.target.value);
+  useEffect(() => {
+    axios.get('/api/genres').then(res=>{setGenres(res.data.data)})
+  },[])
+
+  const onChange = field => evt => {
+    setForm(
+      {...form,
+      [field]: evt.target.value}
+    )
+  }
+
+  const seleciona = value => () => {
+    setForm({
+      ...form,
+      status: value
+    })
   }
 
   const save = () => {
-    axios.post('/api/series', {name: serie}).then((res) =>{
+    axios.post('/api/series', form).then((res) => {
       history.push('/series');
-      });
+    });
   }
 
   return (
@@ -22,10 +39,29 @@ function NewSerie() {
       <form>
         <div className="form-group">
           <label htmlFor="name">Nome:</label>
-          <input type="text" value={serie} onChange={onChange} className="form-control" id="name" />
+          <input type="text" value={form.name} onChange={onChange('name')} className="form-control" id="name" />
         </div>
-        <button type="button" onClick={save} className="btn btn-primary">Salvar</button>
-        <button type="button" onClick={() => history.push('/series')} className="btn btn-danger ml-2">Voltar</button>
+        <div className="form-group">
+          <label htmlFor="genres">Gêneros:</label>
+          <select selected={form.genre} onChange={onChange('genre_id')} className="custom-select custom-select-lg mb-3">
+            <option>Selecione um gênero</option>
+            {
+              genres.map(genre => <option key={genre.id} value={genre.id}>{genre.name}</option>)
+            }
+          </select>
+        </div>
+        <div className="form-group">
+          <div className="custom-control custom-radio">
+            <input type="radio" id="assistindo" checked={form.status === 'Assistindo'} value='assistindo' name="status" className="custom-control-input" onChange={seleciona('Assistindo')} />
+            <label className="custom-control-label" htmlFor="assistindo">Assistindo</label>
+          </div>
+          <div className="custom-control custom-radio">
+            <input type="radio" id="finalizado" checked={form.status === 'Finalizado'} value='finalizado' name="status" className="custom-control-input" onChange={seleciona('Finalizado')} />
+            <label className="custom-control-label mb-3" htmlFor="finalizado">Finalizado</label>
+          </div>
+          <button type="button" onClick={save} className="btn btn-primary">Salvar</button>
+          <button type="button" onClick={() => history.push('/series')} className="btn btn-danger ml-2">Voltar</button>
+        </div>
       </form>
     </div>
   );
